923d6325073f9a3f020be4d5b813f475ccd34372
chocooni <97539061+chocooni@users.noreply.github.com>
2023-10-11 03:54:55 -0500

    Update helper.py

--- helper.py
+++ helper.py
@@ -3,7 +3,7 @@
 import pickle
 import numpy as np
 from keras.datasets import cifar10
-from keras.utils import np_utils
+from tensorflow.python.keras.utils import np_utils
 from matplotlib import pyplot as plt
 import pandas as pd
 import requests
